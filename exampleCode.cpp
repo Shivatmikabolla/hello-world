c118c3d63d28e51c846107f9c53908a21e8f85f2
Shivatmikabolla <151445209+Shivatmikabolla@users.noreply.github.com>
2025-02-04 09:26:05 +0530

    Add files via upload

---
+++ exampleCode.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=3;
+    int b=5;
+    if(a>b){
+        cout<<"a is greater than b";
+    }
+    else{
+        cout<<"a is lesser than b";
+    }
+}
